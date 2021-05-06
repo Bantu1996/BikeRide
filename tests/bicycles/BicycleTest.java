dfcf9a40d8ff8eecc1262dcfaf19154c0eacd393
Bantu1996 <sotyatobantu@gmail.com>
2021-05-06 21:52:17 +0200

    modified the tests folder

--- tests/bicycles/BicycleTest.java
+++ tests/bicycles/BicycleTest.java
@@ -2,9 +2,72 @@ package bicycles;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class BicycleTest {
     @Test
-    public void shouldAccellerate() {
-        assertEquals(1,2);
+    public void shouldAccelerate() {
+
+        Bicycle bicycle = new Bicycle();
+        bicycle.accelerate();
+
+        assertEquals(5, bicycle.currentSpeed());
+    }
+
+    @Test
+    public void shouldAccelerateAndBrakeCorrect() {
+
+        Bicycle bicycle = new Bicycle();
+        bicycle.accelerate();
+        bicycle.brake();
+        assertEquals(2, bicycle.currentSpeed());
+
+    }
+    @Test
+    public void shouldDoMultipleAcceleratesCorrectly() {
+
+        Bicycle bicycle = new Bicycle();
+        bicycle.accelerate();
+        bicycle.accelerate();
+        bicycle.accelerate();
+        assertEquals(15, bicycle.currentSpeed());
+
+    }
+    @Test
+    public void shouldDoMultipleAccelerateAndBrakesCorrectly() {
+
+        Bicycle bicycle = new Bicycle();
+        bicycle.accelerate();
+        bicycle.brake();
+        bicycle.accelerate();
+        bicycle.brake();
+        bicycle.brake();
+        assertEquals(1, bicycle.currentSpeed());
+
+    }
+    @Test
+    public void shouldBeAbleToStop() {
+
+        Bicycle bicycle = new Bicycle();
+        bicycle.accelerate();
+        bicycle.brake();
+        bicycle.accelerate();
+        bicycle.stop();
+        assertEquals(0, bicycle.currentSpeed());
+
+    }
+    @Test
+    public  void shouldBeAbleToAccelerateAfterStopping() {
+        Bicycle bicycle = new Bicycle();
+        bicycle.stop();
+        bicycle.accelerate();
+        assertEquals(5, bicycle.currentSpeed());
+    }
+    @Test
+    public void shouldBeAbleToBreak() {
+        Bicycle bicycle = new Bicycle();
+
+        bicycle.brake();
+        assertEquals(-3, bicycle.currentSpeed());
     }
 }
